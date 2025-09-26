736c82e854e2873a63fceb00495df2f101f6dac3
Yannick <yannickcruz02@gmail.com>
2025-09-26 16:45:57 -0300

    feat: Construção parcial do script que gera selects de unidades

--- Script/converterScript.js
+++ Script/converterScript.js
@@ -13,7 +13,46 @@ document.addEventListener('DOMContentLoaded', () => {
 }
 })
 
-function create_length(){
+function create_select(quantity){
+    const leng_data = [
+        {value: 'm', text: 'metro'},
+        {value: 'km', text: 'quilômetro'},
+        {value: 'cm', text: 'centímetro'},
+        {value: 'mm', text: 'milimetro'},
+        {value: 'um', text: 'micrômetro'},
+        {value: 'nm', text: 'nanômetro'},
+        {value: 'mi', text: 'milha'},
+        {value: 'yd', text: 'jarda'},
+        {value: 'ft', text: 'pés'},
+        {value: 'in', text: 'polegada'},
+        {value: 'ly', text: 'ano-luz'},
+        {value: 'ls', text: 'segundo-luz'}
+    ];
+    const area_data = [
+        {value: 'm2', text: 'metro quadrado'},
+        {value: 'km2', text: 'quilômetro quadrado'},
+        {value: 'cm2', text: 'centímetro quadrado'},
+        {value: 'mm2', text: 'milímetro quadrado'},
+        {value: 'um2', text: 'micrômetro quadrado'},
+        {value: 'ha', text: 'hectare'},
+        {value: 'sqmi', text: 'milha quadrada'},
+        {value: 'sqyd', text: 'jarda quadrada'},
+        {value: 'sqft', text: 'pé quadrado'},
+        {value: 'sqin', text: 'polegada quadrada'},
+        {value: 'ac', text: 'acre'},
+    ];
+    const volume_data = [
+        {value: 'm3', text: 'metro cúbico'},
+        {value: 'km3', text: 'quilômetro cúbico'},
+        {value: 'cm3', text: 'centímetro cúbico'},
+        {value: 'mm3', text: 'milímetro cúbico'},
+        {value: 'um3', text: 'micrômetro cúbico'},
+        {value: 'l', text: 'litro'},
+        {value: 'ml', text: 'mililitro'},
+        {value: 'USG', text: 'galão americano'},
+        {value: 'USQ', text: 'quarto americano'},
+        
+    ]
     const inputs = document.querySelectorAll(".inputs");
     const option_td = document.querySelectorAll("#select-tr td");
     
@@ -27,5 +66,42 @@ function create_length(){
     output_select.id = "output-select";
     option_td[1].appendChild(output_select);
 
+    if(quantity === 'leng'){
+        //const control = document.createElement("option");
+        //control.innerText = "Input";
+        inputs.forEach((input) => {
+            input.setAttribute("placeholder", "metro");
+        });
+        for(const data of leng_data){
+            const option = document.createElement("option");
+
+            option.value = data.value;
+            option.innerText = data.text;
+            option.value === 'm' ? option.setAttribute("selected") : null;
+
+            input_select.appendChild(option);
+            //input_select.appendChild(control);
+
+            const option_clone = option.cloneNode(true);
+            output_select.appendChild(option_clone);
+        }
+    } else if(quantity === 'area'){
+        inputs.forEach((input) => {
+            input.setAttribute("placeholder", "metro quadrado");
+        });
+        for(const data of area_data){
+            const option = document.createElement("option");
+            option.value = data.value;
+            option.innerText = data.text;
+            option.value === 'm' ? option.setAttribute("selected", "selected") : null;
+            input_select.appendChild(option);
+            const option_clone = option.cloneNode(true);
+            output_select.appendChild(option_clone);
+        }
+    } else if(quantity === 'vol'){
+
+    }
     
 }
+
+create_select('area');
